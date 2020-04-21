2a40b85403f34ad7ecb6dff85480fd473cb0f997
trunkslamchest <trunk9slamchest@gmail.com>
2020-04-21 16:56:41 -0400

    fixed serializing issue with create method in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,20 @@
 class UsersController < ApplicationController
 
-	def index
-		@users = User.order(id: :asc)
-		render json: UsersSerializer.new(@users).serialized_json
-	end
+  def index
+    @users = User.order(id: :asc)
+    render json: UsersSerializer.new(@users).serialized_json
+  end
 
   def show
     @user = User.find(params[:id])
-		render json: UsersSerializer.new(@user).serialized_json
+    render json: UsersSerializer.new(@user).serialized_json
   end
 
   def update
     current_user = User.find(params[:id])
     user_update = current_user.update(update_user_params)
     if current_user.valid?
-      render json: {
-                    token: token(current_user.id),
-                    user_id: current_user.id,
-                    user_name: current_user.user_name,
-                    email: current_user.email,
-                    first_name: current_user.first_name,
-                    last_name: current_user.last_name,
-                    gender: current_user.gender,
-                    birth_day: current_user.birth_day,
-                    birth_month: current_user.birth_month,
-                    birth_year: current_user.birth_year,
-                    house_number: current_user.house_number,
-                    street_name: current_user.street_name,
-                    city_town: current_user.city_town,
-                    state: current_user.state,
-                    zip_code: current_user.zip_code
-                   }
+      render json: UsersSerializer.new(current_user).serialized_json
     else
       render json: { errors: current_user.errors.full_messages }, status: :unprocessable_entity
     end
@@ -38,19 +22,17 @@ class UsersController < ApplicationController
 
   def create
     user = User.create(create_user_params)
-		if user.valid?
-			render json: UsersSerializer.new(user).serialized_json
+    if user.valid?
+      render json: UsersSerializer.new(user).serialized_json
     else
       render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-		@user = User.find(params[:id])
-
-		@user.destroy
-
-		render json: @user
+    @user = User.find(params[:id])
+    @user.destroy
+    render json: @user
 	end
 
   private
