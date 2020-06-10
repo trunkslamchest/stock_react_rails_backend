ec25f2bd25c942753bcbe3aa49526b1afce0cbd2
trunkslamchest <trunk9slamchest@gmail.com>
2020-06-10 16:36:45 -0400

    added extra error messages for login_controller.rb

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,8 +4,10 @@ class LoginController < ApplicationController
     user = User.find_by("lower(user_name) = ?", params[:user_name].downcase)
     if user && user.authenticate(params[:password])
       render json: { token: token(user.id), user_id: user.id, user_name: user.user_name }
-    else
-      render json: { errors: [ "Error: Invalid Username or Password" ] }, status: :unprocessable_entity
+    elsif user && !user.authenticate(params[:password])
+      render json: { errors: [ "Error: Invalid Password" ] }, status: :unprocessable_entity
+    elsif !user
+      render json: { errors: [ "Error: Invalid User Name" ] }, status: :unprocessable_entity
     end
   end
 
