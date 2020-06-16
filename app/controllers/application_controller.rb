c0dc24a8d86cbb6c12cd580463d19310efaf3c0f
trunkslamchest <trunk9slamchest@gmail.com>
2020-06-16 11:43:33 -0400

    initial changes to new repo

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,33 @@
+class ApplicationController < ActionController::API
+
+  private
+
+  def token(user_id)
+    payload = { user_id: user_id }
+    JWT.encode(payload, hmac_secret, 'HS256')
+  end
+
+  def hmac_secret
+    ENV["HMAC_SECRET"]
+  end
+
+  def client_has_valid_token?
+    if current_user_id.nil?
+      false
+    else
+      !!current_user_id
+    end
+  end
+
+  def current_user_id
+    begin
+      token = request.headers["Authorization"]
+      decoded_array = JWT.decode(token, hmac_secret, true, { algorithm: 'HS256' })
+      payload = decoded_array.first
+    rescue #JWT::VerificationError
+      return nil
+    end
+    payload["user_id"]
+  end
+
+end
---
+++ app/serializers/users_serializer.rb
@@ -0,0 +1,29 @@
+class UsersSerializer
+  include FastJsonapi::ObjectSerializer
+
+  attributes :user
+
+  attribute :user do |user_obj|
+    {
+      id: user_obj.id,
+      user_name: user_obj.user_name,
+      email: user_obj.email,
+      access: user_obj.access,
+      first_name: user_obj.first_name,
+      last_name: user_obj.last_name,
+      gender: user_obj.gender,
+      birth_day: user_obj.birth_day,
+      birth_month: user_obj.birth_month,
+      birth_year: user_obj.birth_year,
+      house_number: user_obj.house_number,
+      street_name: user_obj.street_name,
+      city_town: user_obj.city_town,
+      state: user_obj.state,
+      zip_code: user_obj.zip_code,
+      join_day: user_obj.created_at.strftime("%e").to_i,
+      join_month: user_obj.created_at.strftime("%B"),
+      join_year: user_obj.created_at.strftime("%Y").to_i,
+    }
+  end
+
+end
